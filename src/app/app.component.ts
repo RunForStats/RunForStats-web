bf66f8dc186f6c9988d15a113521aa44a536b908
blascone <blascone@blascone.it>
2018-01-04 17:12:37 +0100

    dev enviroment customized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, isDevMode, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  isDev = false;
+
+  ngOnInit(): void {
+    this.isDev = isDevMode();
+  }
+
+
 }
